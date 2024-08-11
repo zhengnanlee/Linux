56697f91655ac5a197ad457b204cf92664b9cb22
zhengnanlee <48753271+zhengnanlee@users.noreply.github.com>
2024-08-11 14:10:49 +0800

    Update base_setup.sh

--- base_setup.sh
+++ base_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#设置yum源
+# 设置yum源
 mkdir -p /media/cdrom
 mount /dev/sr0 /media/cdrom
 echo "/dev/sr0                        /media/cdrom              iso9660  defaults        0 0"  >> /etc/fstab
-#有/etc/yum.repos.d/CentOS-Media.repo 文件，无需再写repo文件  
+# 有/etc/yum.repos.d/CentOS-Media.repo 文件，无需再写repo文件  
 
 rpm -ivh /media/cdrom/Packages/wget-1.14-18.el7_6.1.x86_64.rpm
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup 
