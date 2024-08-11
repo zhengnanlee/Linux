e555e54b985bfd402d464c23180a1c92a9a905f7
lizhengnan <lzn122614@vip.qq.com>
2024-08-11 14:21:04 +0800

    设置时区

--- base_setup.sh
+++ base_setup.sh
@@ -14,6 +14,8 @@ yum makecache
 yumi -y update
 yum install -y vim zip unzip lrzsz epel-release
 
+# 设置时区
+timedatectl set-timezone Asia/Shanghai
 
 echo "PS1=\"\[\033[0;32m\]\A \[\033[0;31m\]\u\[\033[0;34m\]@\[\033[0;35m\]\h\[\033[0;34m\]:\[\033[00;36m\]\w\[\033[0;33m\] \n$\[\033[0m\]\"" >> /etc/profile
 echo alias vi=\'vim\'>> /etc/profile
